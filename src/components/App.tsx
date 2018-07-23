a37a9209846b4348b19bc39a2d804881c9e00f8e
Aleksander Dudek <aleksanderd@aleksanderd.media-press.tv>
2018-07-23 17:59:56 +0200

    first push

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Test } from './Test';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>HI!</h1>
+				<Test />
+			</div>
+		);
+	}
+}
---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Test extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Test</p>
+			</div>
+		);
+	}
+}
